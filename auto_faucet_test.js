49206b27ab9c3656c9c10480aca067415f74a04a
Loli Sekai Project <52553776+lolimune@users.noreply.github.com>
2019-07-05 10:47:51 +0800

    Create auto_faucet_test.js

---
+++ auto_faucet_test.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Freebitco.in Auto Faucet Test
+// @github       https://github.com/lolimune
+// @version      0.1
+// @description  Freebitco.in Auto Faucet Test
+// @author       Loli Mune
+// @match        https://freebitco.in/*
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+'use strict';
+if ($('#play_without_captchas_button').is(':visible'))
+    setTimeout(function(){ $('#play_without_captchas_button').click();},2000);
+if ($('#free_play_form_button').is(':visible'))
+    setTimeout(function(){ $('#free_play_form_button').click();},2000);
+if ($('.close-reveal-modal').is(':visible'))
+    setTimeout(function(){ $('.close-reveal-modal').click(); },2000);
+
+//setTimeout(reward.select,1000);
+//setInterval(reward.select,60000);
+})();
